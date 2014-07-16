37d0a751dc59d74c63c864a2b70cfd9f1013840f
fast4y <dzhenko@gmail.com>
2014-07-17 02:34:03 +0300

    Initial commit - created skeleton of the app

---
+++ public/app/app.js
@@ -0,0 +1,16 @@
+// only one app so name is app
+var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    // $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/', {
+            templateUrl: '/partials/home',
+            controller: 'MainCtrl'
+        })
+});
+
+app.controller('MainCtrl', function($scope) {
+    $scope.hello = 'Hi from angular';
+});
