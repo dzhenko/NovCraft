b738dbaba9fc5a1e0b36a4779351b7aa8610e0a6
fast4y <dzhenko@gmail.com>
2014-07-17 10:49:39 +0300

    Preparing for deploy in heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
     mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV || 'development';
-var port = 3030;
+var port = process.env.PORT || 3030;
 
 var app = express();
 
@@ -14,17 +14,23 @@ app.set('views', __dirname + '/server/views/');
 app.use(bodyParser());
 app.use(stylus.middleware({
     src: __dirname + '/public',
-    compile: function(str, path) {
+    compile: function (str, path) {
         return stylus(str).set('filename', path);
     }
 }));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/novCraftDb');
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost/novCraftDb');
+}
+else {
+    mongoose.connect('mongodb://admin:qwerty@ds053109.mongolab.com:53109/novcraft');
+}
+
 var db = mongoose.connection;
 
-db.once('open', function(err) {
-    if(err) {
+db.once('open', function (err) {
+    if (err) {
         console.log('Database could not be opened' + err);
         return;
     }
@@ -32,7 +38,7 @@ db.once('open', function(err) {
     console.log('Database up and running');
 });
 
-db.on('error', function(err) {
+db.on('error', function (err) {
     console.log('Database error ' + err);
 });
 
@@ -43,7 +49,7 @@ var messageSchema = mongoose.Schema({
 var Message = mongoose.model('Message', messageSchema);
 var messageFromDatabase;
 
-Message.remove({}).exec(function(err) {
+Message.remove({}).exec(function (err) {
     if (err) {
         console.log('Messages could not be cleared' + err);
         return;
@@ -51,18 +57,19 @@ Message.remove({}).exec(function(err) {
 
     // adds the message after everything is deleted
     Message.create({ message: 'Hi from Mongoose' })
-        .then(function(model) {
+        .then(function (model) {
             messageFromDatabase = model.message;
         });
 });
 
-app.get('/partials/:partialName', function(req, res) {
+app.get('/partials/:partialName', function (req, res) {
     res.render('partials/' + req.params.partialName);
 });
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.render('index', {message: messageFromDatabase});
 });
 
-console.log('Server running on port '+ port +' ...');
+console.log('Server running on port ' + port + ' ...');
+console.log('Node environment is ' + env);
 app.listen(port);
